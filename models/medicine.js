318aed1d647e9c39d6b08262247e27ac40a18550
Mudassar Rasool <mudassardev16@gmail.com>
2025-09-17 11:42:54 +0500

    Update medicine.js

--- models/medicine.js
+++ models/medicine.js
@@ -5,6 +5,7 @@ const MedicineSchema = new mongoose.Schema({
     generic: { type: String, required: true },
     expiry: { type: Date, required: true },
     quantity: { type: Number, required: true },
+   category: { type: String, required: true },
     purchasePrice: { type: Number, required: true },
     sellingPrice: { type: Number, required: true }
 }, { timestamps: true });
