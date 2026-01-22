905fc995f3424746dc188dba65d56a77da30e7bb
MudassarRassol <mudassardev16@gmail.com>
2026-01-23 01:50:17 +0500

    update

--- app/pages/staff/page.jsx
+++ app/pages/staff/page.jsx
@@ -349,7 +349,7 @@ useEffect(() => {
       {/* Modal */}
       {showModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-          <div className="bg-white rounded-lg p-8 w-[600px] shadow-xl">
+          <div className="bg-white rounded-lg p-8 w-150 shadow-xl">
             <h2 className="text-xl font-bold mb-6 text-center">
               Add New Medicine
             </h2>
